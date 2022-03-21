a78b916bbbb908edb7e4aa6f0c8934d50f0e1002
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-21 20:40:35 +0530

    Create autogen.py

---
+++ autogen.py
@@ -0,0 +1,9 @@
+#get out t.me/devourdevils
+# github.com/devourdevils
+
+
+
+class Translation(object):
+    START_TEXT = """ <b> Hi {} , \nI'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail And Custom Caption support!</b> \n
+<b>Bot Maintained By: @MaI_BoTs </b> \n 
+"""
