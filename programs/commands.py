540fc50874f3835f73eac848516bff39150e6aa9
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-21 20:47:33 +0530

    Update commands.py

--- programs/commands.py
+++ programs/commands.py
@@ -12,25 +12,6 @@ import random
 
 
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
 
     
 @Client.on_message(filters.command(["start"]))
@@ -50,39 +31,11 @@ async def home(client, message):
   reply_markup = InlineKeyboardMarkup(buttons)
   await message.reply_photo(
         photo=random.choice(ALL_PIC),
-        caption=f"""<b> 👋Hello {message.from_user.mention} ,
-        
-</code>Am a telegraph Uploader That Can Upload Photo, Video And Gif
-        
-Simply send me photo, video or gif under 5MB I will upload it to Telegra.ph
-want know more about this bot click help button
-        
-Made With Love By</code> </b> <a href="t.me/devourdevils">DEVOURDEVIL </a>""",
+        caption = START_TEXT ,
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=message.message_id
     )
-
-
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
 
 
 @Client.on_message(filters.command(["help"]))
@@ -101,36 +54,13 @@ async def help(client, message):
   reply_markup = InlineKeyboardMarkup(buttons)
   await message.reply_photo(
         photo=random.choice(ALL_PIC),
-        caption=f"""`hello`  {message.from_user.mention},
-</code>this bot par may be add somany cool and hot fewtures in feuture want know the
-present commands of this bot click or press cmd button
-and Just Send Me A Video/gif/photo under 5mb.
-i'll upload it to telegra.ph and give you the direct link**</code>""",
+        caption = HELP_TEXT ,
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=message.message_id
     )
 
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
 
 @Client.on_message(filters.command(["cmd"]))
 async def cmd(client, message):
